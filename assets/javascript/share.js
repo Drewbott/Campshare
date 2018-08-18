43a04e3031a02c305364c961cb25eeb0beedb16e
Jayhua <jordanhua97@gmail.com>
2018-08-18 11:57:54 -0700

    log in button changes to display name

--- assets/javascript/share.js
+++ assets/javascript/share.js
@@ -9,29 +9,54 @@ var config = {
   firebase.initializeApp(config);
   var db = firebase.database();
   
-
-$("#shareButton").on("click", function(event) { 
-    event.preventDefault();
-    // var name = $("#").val().trim();
-    var location = $("#destination").val().trim();
-    var date = $("#length").val().trim();
-    var numberPeople = $("#spaceAvailable").val().trim();
-    var campingStyle = $("#typeOfSite").val().trim();
-    var notes = $("#notes").val().trim();
-
-    console.log(location)
-
-    var newCampsite = {
-        name: name,
-        location: location,
-        date: date,
-        numberPeople: numberPeople,
-        campingStyle: campingStyle,
-        notes: notes,
-    }
-
-    db.ref().push(newCampsite);
-
+firebase.auth().onAuthStateChanged(function(user) {
+if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    // ...
+    console.log(displayName)
+    console.log(uid) 
+    $("#profileButton").text(displayName);
+    $("#profileButton").attr(href, "#")
+    $("#shareButton").on("click", function(event) { 
+        event.preventDefault();
+        // var name = $("#").val().trim();
+        var location = $("#destination").val().trim();
+        var date = $("#length").val().trim();
+        var numberPeople = $("#spaceAvailable").val().trim();
+        var campingStyle = $("#typeOfSite").val().trim();
+        var notes = $("#notes").val().trim();
     
+        console.log(location)
+        function writeUserData(userId, name, email, imageUrl) {
+            firebase.database().ref('users/' + userId).set({
+              username: displayName,
+              email: email,
+              profile_picture : imageUrl,
+              // Add more stuff here
+            });
+            }
+        var newCampsite = {
+            name: name,
+            location: location,
+            date: date,
+            numberPeople: numberPeople,
+            campingStyle: campingStyle,
+            notes: notes,
+        }
+    
+        db.ref('users/' + uid).push(newCampsite);
+    
+    })
+} else {
+    // User is signed out.
+    // ...
+}
+});
 
-})
