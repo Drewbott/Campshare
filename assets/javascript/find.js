615fa896f62d29480449fa49f240eee21a05baa7
James Albert Phillips V <jamesalbertphillipsv@jamess-air.attlocal.net>
2018-08-20 21:12:05 -0700

    add function and conditionals to recognize on click functions

--- assets/javascript/find.js
+++ assets/javascript/find.js
@@ -30,16 +30,91 @@ var config = {
     displayParkInfo();
   })
 
-  function displayParkInfo() {
+
+
+//   function displayParkInfo() {
     
-    var queryURL_NPS = "https://developer.nps.gov/api/v1/campgrounds?q=yosemite&api_key=5cZGKFkWtjCEhTA6R4fLucsuyuPYAkLhmZgeIYbw&limit=100&fields=latLong"
+//     var queryURL_NPS = "https://developer.nps.gov/api/v1/campgrounds?q=yosemite&api_key=5cZGKFkWtjCEhTA6R4fLucsuyuPYAkLhmZgeIYbw&limit=100&fields=latLong"
 
-    $.ajax({
+//     $.ajax({
 
-      url: queryURL_NPS,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response.data)
-      console.log(response.data[0].name)
+//       url: queryURL_NPS,
+//       method: "GET"
+//     }).then(function(response) {
+//       console.log(response.data)
+//       console.log(response.data[0].name)
+//     })
+//   }
+
+
+  $('#my-select').on('change', function(){
+    var e = document.getElementById("my-select");
+    var value = e.options[e.selectedIndex].value;
+    console.log(value);
+    var park
+    if (value == 1){
+      park = 'yose';
+  
+    } else if (value == 2){
+        park = 'yose';
+    }
+    else if (value == 3){
+        park = 'yose';
+    }
+    else if (value == 4){
+        park = 'yose';
+    }
+    else if (value == 5){
+        park = 'yose';
+    }
+    else if (value == 6){
+        park = 'yose';
+    }
+    else if (value == 7){
+        park = 'yose';
+    }
+    else if (value == 8){
+        park = 'yose';
+    }
+    else if (value == 9){
+        park = 'yose';
+    }
+    var queryURL = `https://developer.nps.gov/api/v1/parks?parkCode=${park}&api_key=5cZGKFkWtjCEhTA6R4fLucsuyuPYAkLhmZgeIYbw&limit=100`
+    $.get(queryURL,function(response) {
+        //use reponse to populate divs
+        console.log(response.data[0].name)
+        $(".park-title").text(response.data[0].name)
+        database.ref('/posts').orderByChild('location').equalTo(park).on("value", (snapshot) => {
+            $('.park-title').empty()
+            snapshot.forEach(data => {
+                console.log(data)
+            //   var p = $('<p>')
+            //   p.text(data.val().campingStyle)
+            //  console.log(data.val().campingStyle)
+            //   $('.park-title').append(p)
+            })
+          })
     })
-  }
+  })
+
+//   $(".dropdown").onclick(function dropdownSelect() {
+//     var createDiv = $("<div class='findSite jumbotron'>")
+//     createDiv.text(campsiteName)
+//   })
+
+
+  //     $(".camp-spot").attr(campDestination)
+//     $('.camp-requirements').attr(displayName + campingStyle + notes + numberPeople)
+
+//     database.ref('users/' + uid).pull(newCampsite);
+
+//   });
+
+//   var ref = database.ref("server/saving-data/fireblog/posts");
+
+// // Attach an asynchronous callback to read the data at our posts reference
+// ref.on("value", function(snapshot) {
+//   console.log(snapshot.val());
+// }, function (errorObject) {
+//   console.log("The read failed: " + errorObject.code);
+// });
