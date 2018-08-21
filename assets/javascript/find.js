16f0880d20fa2b588cb7946b77d24430f4122ae6
Jayhua <jordanhua97@gmail.com>
2018-08-21 12:35:20 -0700

    find page populates

--- assets/javascript/find.js
+++ assets/javascript/find.js
@@ -81,25 +81,48 @@ var config = {
     recenter(value)
 
     
-    var queryURL = `https://developer.nps.gov/api/v1/parks?parkCode=${park}&api_key=5cZGKFkWtjCEhTA6R4fLucsuyuPYAkLhmZgeIYbw&limit=100`
+    var queryURL = `https://developer.nps.gov/api/v1/parks?parkCode=${park}&api_key=5cZGKFkWtjCEhTA6R4fLucsuyuPYAkLhmZgeIYbw&limit=100&fields=images`
     $.get(queryURL,function(response) {
         //use reponse to populate divs
         console.log(response.data[0].name)
         $(".park-title").text(response.data[0].name)
-        database.ref('/posts').orderByChild('location').equalTo(park).on("value", (snapshot) => {
+        database.ref('posts/').orderByChild('location').equalTo(park).on("value", (snapshot) => {
             $('.park-title').empty()
+            console.log('CHICKEN');
+            console.log(snapshot.val())
 
-            //creates a button to send to the host's email
-            $(".park-title").append('<button onclick=location.href="mailto:' + userEmail + '">Send Email to Host</button>')
+            snapshot.forEach(function(childSnap){
+              campDiv = $("<div>")
+              campDiv.attr("class","card")
+              campDiv2 = $("<div>")
+              campDiv2.attr("class","row")
+              campDiv3 = $("<div>")
+              campDiv3.attr("class","col-md-3")
 
+              campDiv3img = $("<img>")
+              campDiv3img.attr("src", response.data[0].images[0].url) // call snapshot.val for camp imgUrl
+              campDiv3img.attr("class","card-img-top")
+              campDiv3.append(campDiv3img)
+              campDiv3.append("<p>" + childSnap.val().name + " wants to share their site at with "  + childSnap.val().numberPeople + " from " + childSnap.val().startDate + " to " + childSnap.val().endDate + ". " + childSnap.val().name + " camping style is " + childSnap.val().campingStyle + " & wants you to bring " + childSnap.val().requirements + ".")
+              //creates a button to send to the host's email
+              campDiv3.append('<button onclick=location.href="mailto:' + childSnap.val().userEmail + '">Send Email to Host</button>')
 
-            snapshot.forEach(data => {
-                console.log(data)
-            //   var p = $('<p>')
-            //   p.text(data.val().campingStyle)
-            //  console.log(data.val().campingStyle)
-            //   $('.park-title').append(p)
+              weatherDiv =
+              $(".park-title").append(campDiv, campDiv2, campDiv3)
+              console.log(childSnap.val().name)
             })
+            
+
+
+
+
+            // snapshot.forEach(data => {
+            //     // console.log(data)
+            // //   var p = $('<p>')
+            // //   p.text(data.val().campingStyle)
+            // //  console.log(data.val().campingStyle)
+            // //   $('.park-title').append(p)
+            // })
           })
     })
   })
